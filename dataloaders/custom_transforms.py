1d911f663bb747de908f147b7a12deaa390c93f2
Wugengxian <60342704+Wugengxian@users.noreply.github.com>
2021-05-11 15:36:59 +0800

    Update custom_transforms.py

--- dataloaders/custom_transforms.py
+++ dataloaders/custom_transforms.py
@@ -1,23 +1,68 @@
 import torch
-from torchsample.transforms import Rotate, RandomTranslate, Flip, ToTensor, Compose, RandomAffine
 import numpy as np
+import random
 
-class CustomRotate(object):
-    def __init__(self, angle = 90):
-        self.transforms = Compose([Rotate(angle)])
+class RamdomRotate(object):
+
     def __call__(self, sample):
         
-        md = np.array(sample['md'])
-        fa = np.array(sample['fa'])
-        mask = np.array(sample['mask'])
+        md = sample['md']
+        fa = sample['fa']
+        mask = sample['mask']
         label = sample['label']
-        md = torch.from_numpy(md).float()
-        for i in range(0, md.shape[0]) : md[i] = self.transforms(md[i])
-        fa = torch.from_numpy(fa).float()
-        for i in range(0, fa.shape[0]) : fa[i] = self.transforms(fa[i])
-        mask = torch.from_numpy(mask).float()
-        for i in range(0, mask.shape[0]) : mask[i] = self.transforms(mask[i])
-        label = torch.Tensor([label]).long()
+        angle = random.randint(0, 3)
+        axe = random.randint(0, 2)
+        if axe == 0:
+            axes = (0, 1)
+        elif axe == 1:
+            axes = (1, 2)
+        elif axe == 2:
+            axes = (0, 2)
+        md = np.rot90(md, angle, axes=axes)
+        fa = np.rot90(fa, angle, axes=axes)
+        mask = np.rot90(mask, angle, axes=axes)
+        return {'md': md, 'fa': fa, 'mask': mask, "label": label}
+
+class RamdomFlip(object):
+
+    def __call__(self, sample):
+        
+        md = sample['md']
+        fa = sample['fa']
+        mask = sample['mask']
+        label = sample['label']
+        angle = random.randint(0, 2)
+        md = np.flip(md, angle)
+        fa = np.flip(fa, angle)
+        mask = np.flip(mask, angle)
+        return {'md': md, 'fa': fa, 'mask': mask, "label": label}
+    
+class RamdomFlip(object):
+
+    def __call__(self, sample):
+        
+        md = sample['md']
+        fa = sample['fa']
+        mask = sample['mask']
+        label = sample['label']
+        angle = random.randint(0, 1)
+        md = np.flip(md, angle)
+        fa = np.flip(fa, angle)
+        mask = np.flip(mask, angle)
+        return {'md': md, 'fa': fa, 'mask': mask, "label": label}
+    
+class RamdomMask(object):
+
+    def __call__(self, sample):
+        
+        md = sample['md']
+        fa = sample['fa']
+        mask = sample['mask']
+        label = sample['label']
+        if random.randint(0, 1) == 0:
+            md = md * mask
+        if random.randint(0, 1) == 0:
+            fa = fa * mask
         return {'md': md, 'fa': fa, 'mask': mask, "label": label}
 
 
@@ -28,9 +73,9 @@ class ToTensor(object):
         # swap color axis because
         # numpy image: H x W x C
         # torch image: C X H X W
-        md = sample['md']
-        fa = sample['fa']
-        mask = sample['mask']
+        md = np.ascontiguousarray(sample['md'])
+        fa = np.ascontiguousarray(sample['fa'])
+        mask = np.ascontiguousarray(sample['mask'])
         label = sample['label']
         md = torch.from_numpy(md).float()
         fa = torch.from_numpy(fa).float()
